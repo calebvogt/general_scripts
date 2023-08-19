a681849792bb374264595f480436b80adf0ba00a
Caleb C. Vogt <calebvogt@outlook.com>
2023-08-18 20:11:35 -0400

    Update simba_input_csv_interpolation.R

--- R_scripts/simba_input_csv_interpolation.R
+++ R_scripts/simba_input_csv_interpolation.R
@@ -1,39 +1,31 @@
-# CM_simba_interpolation.R
 ## Created by Caleb C. Vogt, PhD Candidate @ Cornell University
-
-# load packages
 library(tidyverse)
-library(readxl)
-library(plyr)
-library(dplyr)
 library(data.table)
-library(readr)
-library(lubridate)
 library(zoo)
+library(beepr)
 
-# SET WD, LOAD DATA, FORMAT TIME SERIES
-wd <- setwd("Z:/15_CM_Female_Preference_Tracking/SLEAP_CM_Female_Preference_v3/simba_v3/project_folder/csv/input_csv")
-# wd <- setwd("Z:/15_CM_Female_Preference_Tracking/SLEAP_CM_Female_Preference_v3/simba_v3/project_folder/csv/input_csv/test")
-dir.create("R_proc")
-out_fp = "Z:/15_CM_Female_Preference_Tracking/SLEAP_CM_Female_Preference_v3/simba_v3/project_folder/csv/input_csv/R_proc"  #### CHANGE THIS
-# out_fp = "Z:/15_CM_Female_Preference_Tracking/SLEAP_CM_Female_Preference_v3/simba_v3/project_folder/csv/input_csv/test/R_proc"  #### CHANGE THIS
-
-## LIST DATA
-filenames <- list.files(wd, pattern = "*.csv")
-
-aa = 1
+## testing
+wd <- setwd("Y:/Data/FieldProject/video_behavior_sleap/2_ephys_rig_low_quality/simba/project_folder/csv/input_csv")
+filenames <- list.files(wd,pattern="*.csv")
+aa=1
 for(aa in 1:length(filenames)){
-  
-  df <- fread(filenames[aa], header = FALSE)
-  df[df == 0 ] <- NA
-  df[df == "0.0" ] <- NA
-    # replace frame 0 NA with 0 again
-  df[4,1] <- 0
- 
-  headers <- df[1:3,]
-  df2 <- df[-c(1:3),]
-  df3 <- zoo::na.fill(df2, "extend")
-  df4 <- rbind(headers, df3)
-  
-  write.table(df4, paste0(out_fp, "/", filenames[aa]), row.names = FALSE, col.names = FALSE, sep =",")
+  print(paste("Converting file",filenames[[aa]]))
+  df <- fread(filenames[aa],header=T,skip=2) ## read in the file, drop the any header rows added by simba
+  df[df==0] <- NA
+  df[df=="0.0"] <- NA # replace frame 0 NA with 0 again
+  df[1,1] <- 0 ## replace frame 0 NA with 0 
+  x_columns <- grep("_x$",colnames(df)) ## Get the indices of columns ending with '_x'
+  first_row <- NA
+  for (i in 1:nrow(df)) { ## find the first row within the '_x' cols that is not NA, then break the loop. 
+    if (any(!is.na(df[i,..x_columns]))) {
+      first_row <- i
+      break
+    }
+  }
+  sub_df <- df[first_row:nrow(df),] ## get rows where mouse is detected 
+  filled_df <- as.data.table(zoo::na.fill(sub_df, "extend")) ## smooth linear interpolation of mouse rows
+  df[first_row:nrow(df),] <- filled_df ## rejoin interpolated data with the initial empty frames. 
+  colnames(df)[1] <- "" ## make empty cell for first column name to help with simba reading. 
+  write.table(df,paste0(wd,"/",filenames[aa]),row.names=F,col.names=T,sep =",")
+  beep(1)
 }
